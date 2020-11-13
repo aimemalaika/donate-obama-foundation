fcee841270f9ec4a0f89d18b1122d471c369a10b
Aime Malaika <aimemalaika1995@gmail.com>
2020-11-13 16:40:36 +0200

    p2

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+$('.donation-month-details').click(function () {
+    $('#modal-container').removeAttr('class').addClass('one');
+    $('body').addClass('modal-active');
+})
+
+$('#modal-container').click(function () {
+    $(this).addClass('out');
+    $('body').removeClass('modal-active');
+});
