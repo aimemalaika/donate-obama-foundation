73d06d81f0649620f685c1087bdc202061704926
Aime Malaika <aimemalaika1995@gmail.com>
2020-11-14 10:27:08 +0200

    e

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,3 +39,41 @@ $('.donationperprice').click(function(){
         window.location.href = "pay.html";
     }, 2000);
 })
+
+paypal.Button.render({
+    // Configure environment
+    env: 'sandbox',
+    client: {
+    sandbox: 'demo_sandbox_client_id',
+    production: 'demo_production_client_id'
+    },
+    // Customize button (optional)
+    locale: 'en_US',
+    style: {
+    size: 'small',
+    color: 'gold',
+    shape: 'pill',
+    },
+
+    // Enable Pay Now checkout flow (optional)
+    commit: true,
+
+    // Set up a payment
+    payment: function(data, actions) {
+    return actions.payment.create({
+        transactions: [{
+        amount: {
+            total: '0.01',
+            currency: 'USD'
+        }
+        }]
+    });
+    },
+    // Execute the payment
+    onAuthorize: function(data, actions) {
+    return actions.payment.execute().then(function() {
+        // Show a confirmation message to the buyer
+        window.alert('Thank you for your purchase!');
+    });
+    }
+}, '#paypal-button');
